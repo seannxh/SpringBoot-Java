c31b8bd39933aacecbf55dad5db33e1c507ba711
seannxh <nohsean6@gmail.com>
2025-04-22 00:14:43 -0500

    Notes

---
+++ src/main/java/com/learningjava/learn_spring_framework/Notes.java
@@ -0,0 +1,17 @@
+package com.learningjava.learn_spring_framework;
+
+
+///Spring Stereotype Annotations
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+
+// @Component - General Annotation Applicable for any class
+//- Base for all Spring Stereotype Annotations
+//
+//-@Service  Indicates that an annotated class has business logic
+//-@Controller indicates that an annotated class is a Controller (Web Controller)
+//-@Repository Indicates that an annotated class is used to retrieve and or manipulate Data in a database
